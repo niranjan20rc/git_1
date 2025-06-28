ada1dfdb7ebafd3b8aa9dac8dfa3919486096f12
niranjanrc20 <niranjanrc20@gmail.com>
2025-06-28 12:24:50 +0530

    changes in nodemon

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.use(express.json())
 
 
+
 // DB 
 mongoose.connect(LINK).then(()=>{console.log("DB CREATED")}).catch((e)=>{console.log(e.message)});
 const my_schema = mongoose.Schema({
